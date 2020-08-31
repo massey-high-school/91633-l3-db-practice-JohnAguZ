65250448ce38a36fba1f6cb2f2cf3a8b243e8155
JohnAguZ <Aguirrezabalaj69731@masseyhigh.school.nz>
2020-09-01 10:29:03 +1200

    Create config.php
    Config File

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define ("DB_NAME","aguirrezabalaj");
+define ("DB_USERNAME","_aguirrezabalaj");
+define("DB_PASSWORD", "18015789e75348928b08ba782beb2545");
+define ("DB_HOST", "localhost");
+
+?>
+    
